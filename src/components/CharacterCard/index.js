76825e905fe739f5eaf0cc1784697b77564ef8a5
Matt Boatman <matthewd.boatman@gmail.com>
2018-06-13 22:55:55 -0400

    add health indicator on character card

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import HealthIndicator from '../HealthIndicator';
 
 class CharacterCard extends Component {
-    render() {
-        return (
-            <div>
-                poop
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Paper elevation={4}>
+        <Typography variant="headline" component="h3">
+          Tug FireBeard
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your
+          application.
+        </Typography>
+        <HealthIndicator />
+      </Paper>
+    );
+  }
 }
 
-CharacterCard.propTypes = {
-
-};
+CharacterCard.propTypes = {};
 
 export default CharacterCard;
--- src/components/HealthIndicator/index.js
+++ src/components/HealthIndicator/index.js
@@ -6,12 +6,14 @@ import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
 
 const propTypes = {
-  maxHP: PropTypes.number.isRequired,
-  currentHP: PropTypes.number.isRequired,
+  maxHP: PropTypes.number,
+  currentHP: PropTypes.number,
   temporaryHP: PropTypes.number,
 };
 
 const defaultProps = {
+  maxHP: 10,
+  currentHP: 7,
   temporaryHP: 0,
 };
 
@@ -19,39 +21,44 @@ const getCurrentPercentage = (current, max) => {
   return 100 / (max / current);
 };
 
-
 const HealthIndicator = ({ classes, maxHP, currentHP, temporaryHP }) => {
   return (
-    <Wrapper>
+    <HealthIndicatorWrap>
       <Typography
         className={classes.root}
         variant="caption"
       >{`${currentHP}/${maxHP}`}</Typography>
-      <LinearProgress
-        classes={{
-          bar: classes.bar,
-          colorSecondary: classes.colorSecondary,
-        }}
-        className={classes.bar}
-        variant="buffer"
-        color="secondary"
-        value={getCurrentPercentage(currentHP, maxHP)}
-        valueBuffer={getCurrentPercentage(currentHP + temporaryHP, maxHP)}
-      />
-    </Wrapper>
+      <Wrapper>
+        <LinearProgress
+          classes={{
+            bar: classes.bar,
+            colorSecondary: classes.colorSecondary,
+          }}
+          className={classes.bar}
+          variant="buffer"
+          color="secondary"
+          value={getCurrentPercentage(currentHP, maxHP)}
+          valueBuffer={getCurrentPercentage(currentHP + temporaryHP, maxHP)}
+        />
+      </Wrapper>
+    </HealthIndicatorWrap>
   );
 };
 
+const HealthIndicatorWrap = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
 const Wrapper = styled.div`
   box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.15);
+  width: 100%;
+  display: inline-block;
 `;
 const styles = {
   root: {
-    position: 'absolute',
-    zIndex: 200,
-    top: 8,
-    left: 10,
-    color: 'hsl(0, 99%, 88%)',
+    display: 'inline-block',
+    padding: '0 5px',
   },
   bar: {
     height: 17,
